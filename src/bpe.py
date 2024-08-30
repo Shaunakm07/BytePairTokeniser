2c6894853fd3b51003803f0d2628ba1e1e70f72b
shaunak <shaunak.mohapatra@gmail.com>
2024-08-30 19:23:01 +0100

    finally trained "byte" pair tokeniser

--- src/bpe.py
+++ src/bpe.py
@@ -1,96 +1,52 @@
-import regex as re 
+import regex as re
 import time
 
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-text = "low low low low low lower lower widest widest widest newest newest newest newest newest newest"
 
-def pre_tokenise(text):
-    return re.findall(PAT, text)
-
-def create_corpus(special_tokens):
-    corpus = special_tokens
-
-    for i in range(256):
-        char = chr(i)
-        corpus.append(char)
-    return corpus
-
-def get_word_counts(words):
-    word_counts = {}
-    for word in words:
-        w = tuple([chr(c) for c in list(word.encode("utf-8"))])
-        count = word_counts.get(w, 0)
-        count += 1
-        word_counts[w] = count
-    return word_counts
-
-def find_byte_pairs(word_count):
-    word_keys = word_count.keys()
-    byte_pairs = {}
-
-    for k in word_keys:
-        for i in range(len(k) - 1):
-            pairs = (k[i], k[i + 1])
-            current_count = byte_pairs.get(pairs, 0)
-            weight = word_count[k]
-            current_count += weight
-            byte_pairs[pairs] = current_count
-    
-    return byte_pairs
-
-def find_lexically_highest(byte_pairs):
-    max_count = max([v for k,v in byte_pairs.items()])
-    max_pairs = []
-
-    for k,v in byte_pairs.items():
-        if v == max_count:
-            max_pairs.append(k)
-
-    heighest_lexical = max(max_pairs)
-    
-    return "".join(heighest_lexical), heighest_lexical
-
-def merge_word_count(word_counts, heighest_lexical):
-    new_word_counts = {}
-
-    for k,v in word_counts.items():
-        k = list(k)
-        i = 0
-        while i < (len(k) - 1):
-            pairs = "".join(k[i] + k[i + 1])
-            if pairs == heighest_lexical:
-                k[i] = heighest_lexical
-                k.pop(i + 1)
-                i += 1
-            i += 1
-        new_word_counts[tuple(k)] = v
-    return new_word_counts
-        
-def train_tokeniser(file, vocab_size, special_tokens):
-    f = open(file, "r")
-    text = f.read()
+def train_bpe(input_file, vocab_size, special_tokens):
+    pre_tokensied = re.findall(PAT, open(input_file).read())
     merges = []
-
-    pre_tokenised = pre_tokenise(text)
-    corpus = create_corpus(special_tokens)
-    word_counts = get_word_counts(pre_tokenised)
+    vocab = special_tokens
+    for i in range(256):
+        vocab.append(chr(i).encode("utf=8"))
     
-    while len(corpus) < vocab_size:
-        byte_pairs = find_byte_pairs(word_counts)
+    word_counts = {}
+    for w in pre_tokensied:
+        w = tuple(c.encode() for c in (list(w)))
+        word_counts[w] = word_counts.get(w, 0) + 1
 
-        heighest_lexical, merge = find_lexically_highest(byte_pairs)
-        merges.append(merge)
-        corpus.append(heighest_lexical)
-    
-        merged = merge_word_count(word_counts, heighest_lexical)
-        word_counts = merged
+    while len(vocab) < vocab_size:
+        byte_pairs = {} 
+        for k,v in word_counts.items():
+            for k1, k2 in zip(k, k[1:]):
+                pairs = (k1,k2)
+                byte_pairs[pairs] = byte_pairs.get(pairs, 0) + v
 
-    return ({i:c for i,c in enumerate(list(set(corpus)))}, merges)
+        max_val = max([v for k,v in byte_pairs.items()])
+        max_list = []
+        for k,v in byte_pairs.items():
+            if v == max_val:
+                max_list.append(k)
 
+        heighest_lexical = max(max_list)
+        merges.append(heighest_lexical)
+        joined = heighest_lexical[0] + heighest_lexical[1]
+        vocab.append(joined)
 
-start_time = time.time()
-vocab, merges = train_tokeniser("text.txt", 500, [])
+        new_word_counts = {}
+        for k,v in word_counts.items():
+            word = list(k)
+            i  = 0
+            while i < len(word) - 1:
+                pairs = (word[i], word[i + 1])
+                if pairs == heighest_lexical:
+                    word[i] = joined
+                    word.pop(i + 1)
+                    i += 1
+                i+= 1
+            new_word_counts[tuple(word)] = v
+        word_counts = new_word_counts
+    return {i:s for i,s in enumerate(vocab)}, merges
+
+vocab, merges = train_bpe("text.txt", 263, ["<token>"])
 print(merges)
-end_time = time.time()
-
-print(end_time - start_time)
