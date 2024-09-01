9dfd7b4face4ac1a1fa85ba9d2670697a0252d60
shaunak <shaunak.mohapatra@gmail.com>
2024-09-01 12:12:49 +0100

    completed tokeniser that can encode and decode documents with a compression ratio of 2 on sample text 🥳

--- src/bpe.py
+++ src/bpe.py
@@ -4,7 +4,14 @@ import time
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
 def train_tokeniser(input_file, vocab_size, special_tokens):
-    pre_tokensied = re.findall(PAT, open(input_file).read())
+    text = open(input_file).read()
+    text_list = text.split(" ")
+    
+    filtered_text = []
+    for word in text_list:
+        if word not in special_tokens:
+            filtered_text.append(word)
+    pre_tokensied = re.findall(PAT, " ".join(filtered_text))
     merges = []
     vocab = special_tokens
     for i in range(256):
@@ -30,6 +37,7 @@ def train_tokeniser(input_file, vocab_size, special_tokens):
             if v == max_val:
                 max_list.append(k)
         heighest_lexical = max(max_list)
+
         merges.append(heighest_lexical)
         joined = heighest_lexical[0] + heighest_lexical[1]
         vocab.append(joined)
@@ -67,6 +75,79 @@ def train_tokeniser(input_file, vocab_size, special_tokens):
 
     return {i:s for i,s in enumerate(vocab)}, merges
 
+class tokeniser():
+    def __init__(self, vocab, merges, special_tokens):
+        self.vocab = vocab
+        self.reverse_vocab = {s:i for i,s in vocab.items()}
+        self.merges = merges
+        self.special_tokens = special_tokens
 
-vocab, merges = train_tokeniser("text.txt", 500, ["<token>"])
-print(merges)
+    def load_text(self, input_file):
+        text = open(input_file).read()
+        text_list = text.split(" ")
+    
+        filtered_text = []
+        special_indexes = {}
+        for i,word in enumerate(text_list):
+            if word not in special_tokens:
+                filtered_text.append(word)
+            else:
+
+                special_indexes[i] = word
+        pre_tokensied = re.findall(PAT, " ".join(filtered_text))
+        return pre_tokensied, special_indexes
+    
+    def compute_bytes(self, word):
+        byte_words = {}
+        for wor in word:
+            w = list(c.encode() for c in (list(wor)))
+            for m in self.merges:
+                joined = m[0] + m[1]
+                i = 0
+                while i < len(w) - 1:
+                    pairs = (w[i], w[i + 1])
+                    if pairs == m:
+                        w[i] = joined
+                        w.pop(i + 1)
+                        i += 1
+                    i += 1
+
+            byte_word = []
+            for byte in w:
+                try:
+                    byte_word.append(self.reverse_vocab[byte])
+                except:
+                    pass
+            byte_words[wor] = byte_word
+        return byte_words
+
+    def encode(self, input_file):
+        pretokenised, special_indexes = self.load_text(input_file)
+        words = list(set(pretokenised))
+        byte_words = self.compute_bytes(words)
+        tokenised_list = []
+
+        for k,v in special_indexes:
+            pretokenised.insert(k, v)
+        for tok in pretokenised:
+            tokenised_list += byte_words[tok]
+        
+        return tokenised_list
+
+    def decode(self, encoded):
+        text = b""
+        for token in encoded:
+            text += vocab[token]
+        return text
+
+# compute bpe vocab
+starttime = time.time()
+special_tokens = []
+vocab, merges = train_tokeniser("text.txt", 500, [])
+endtime = time.time()
+print(f"total time for creating vocab: {str(endtime - starttime)}")
+
+tknsr = tokeniser(vocab, merges, special_tokens)
+encoded = tknsr.encode("text.txt")
+decoded = tknsr.decode(encoded)
+print(f"tokeniser compression ratio: {str(len(list(decoded)) / len(encoded))}")
